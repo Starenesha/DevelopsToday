2660b23009b50484184a745b5eb857a8d72d5ebf
Starenesha <olgastarynenko@gmail.com>
2020-05-30 09:49:45 +0300

    some changes

--- News_Board/settings.py
+++ News_Board/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = '6379'
